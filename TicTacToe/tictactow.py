e80cb266b74ac40aa88fa3b992a953406021af25
Reyner Veliz <reynerv@yahoo.com>
2023-06-21 00:16:57 -0600

    Game idea brainstorm

--- TicTacToe/tictactow.py
+++ TicTacToe/tictactow.py
@@ -11,3 +11,83 @@ greeting_message = """
 ***********************************************************************
                        Welcome to the game!  
 """
+
+############## PLAYER
+## Attributes:
+    # name
+    # tile
+    # points
+    # is_active
+## Methods
+    # add_point
+    # switch_active
+    # reset_points
+
+
+
+############## Game
+## Attributes:
+    # board
+    # player_1
+    # player_2
+## Methods:
+    # check_if_winner
+    # check_if_tie
+    # reset_board
+    # clear_screen
+
+
+
+# Def Board
+
+# Print Greeting
+
+# Define player 1 and tile
+# Define player 2 and tile
+
+# Randomly Choose First Player
+
+# Clear Screen
+# Show Score
+# Print Board
+# Show available positions
+# Select position
+
+# Check if winner 
+
+# If winner:
+    # Add point to active player
+    # Ask if continue playing
+    # if yes
+        # Reset Board
+        # clear screen
+        # random player
+        # print score
+        # print board
+    # if no:
+        # Exit game
+        # print exit message and score
+# If no winner:
+    # clear screen
+    # reverse players
+    # print score
+    # print board
+
+# If Tie:
+    # Ask if continue playing
+    # If yes:
+        # Reset Board
+        # clear screen
+        # random player
+        # print score
+        # print board
+    # If no:
+        # exit game
+        # print exit message and score
+# If no Tie:
+    # clear screen
+    # reverse players
+    # print score
+    # print board
+
+
